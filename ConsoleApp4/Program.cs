bbda48d032b7f6ae04ad4861de435ea8b610dba1
Julia Dubas <uliadubas94@gmail.com>
2022-07-30 13:17:05 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double FirstNumber, SecondNumber;
+
+            string decision;
+
+            Console.WriteLine("Enter the first number");
+
+            FirstNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+
+            SecondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter an operation: '+', '-', '*', '/' ");
+
+            decision = Console.ReadLine();
+
+            if (decision == "+") 
+            {             
+
+                Console.WriteLine(FirstNumber + SecondNumber);
+            }
+
+            else if(decision == "-")
+            {
+             
+                Console.WriteLine(FirstNumber - SecondNumber);
+            }
+            else if(decision == "*")
+            {
+                
+
+                Console.WriteLine(FirstNumber * SecondNumber);
+            }
+            else if(decision == "/")
+            {
+
+                Console.WriteLine(FirstNumber / SecondNumber);
+            }
+        }
+    }
+}
+ 
